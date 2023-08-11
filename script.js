bf1897929b0a5a05ed1949f623e61541d3be48c8
Allegorylla <138933736+JustinDerengo@users.noreply.github.com>
2023-08-11 11:05:24 -0600

    Tests Complete, Need to finish Script.js

--- script.js
+++ script.js
@@ -4,13 +4,44 @@ window.addEventListener("load", function() {
 
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
+    let listedPlanetsResponse = myFetch();
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
         console.log(listedPlanets);
-    }).then(function () {
+    })
+    .then(function () {
         console.log(listedPlanets);
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+
     })
+    let destination = pickPlanet(listedPlanets)
+    console.log(destination)
+    addDestinationInfo(document, destination.name, destination.diameter, destination.star, destination.distance, destination.moons, destination.imageUrl);
+    
+
+    let form = document.querySelector("form")
+    form.addEventListener("submit", function (event) {
+        let pilotName = document.querySelector("input[name=pilotName]").value;
+        let copilotName = document.querySelector("input[name=copilotName]").value;
+        let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+        let cargoMass = document.querySelector("input[name=cargoMass]").value;
+        let list = document.getElementById("faultyItems");
+        event.preventDefault();
+
+        if (validateInput(fuelLevel) === "Not a Number" || validateInput(fuelLevel) === "Empty") {
+            alert("Invalid Input for Fuel Level")
+        }            
+        if (validateInput(cargoMass) === "Not a Number" || validateInput(cargoMass) === "Empty") {
+            alert("Invalid Input for Cargo Mass")
+        }
+        if (validateInput(pilotName) === "Is a Number" || validateInput(pilotName) === "Empty") {
+            alert("Invalid Input for Pilot")
+        }
+        if (validateInput(copilotName) ==="Empty" || validateInput(pilotName) === "Is a Number") {
+            alert("Invalid Input for Co-pilot")
+        }
+        formSubmission(document, list, pilotName, copilotName, fuelLevel, cargoMass);
+    }
+    )
     
  });
